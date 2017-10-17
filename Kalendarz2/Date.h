fc2e603d8a1169f78940942a52eb8d49d85040a0
MonikaKrol <krol.monika.k@gmail.com>
2017-10-17 17:00:08 +0200

    Update Date.h

--- Kalendarz2/Date.h
+++ Kalendarz2/Date.h
@@ -1,53 +1,41 @@
-/*
- * Date.hpp
- *
- *  Created on: 10-10-2017
- *      Author: Wawryka, Warzecha, Król
- */
-
 #ifndef KALENDARZ_Date_H
 #define KALENDARZ_Date_H
 
+//iostream potrzebne?
 #include <iostream>
+//może lepiej dać cstdlib::coś, co tego używa?
 #include <cstdlib>
+//ostream potrzebne?
 #include <ostream>
-//using namespace std;
 
 class Date
 {
-private:
-	int year;
-	int month;
-	int day;
+	private:
+		int year;
+		int month;
+		int day;
 
-	friend Date operator+ (int offset, Date &old_Date);
-public:
+		friend Date operator+ (int offset, Date &old_Date);
+	public:
 
-	Date (int year = 1970, int month = 1, int day = 1);
-	Date (const Date &old_Date);
+		Date (int year = 1970, int month = 1, int day = 1);
+		Date (const Date &old_Date);
 
-	int getYear();
-	int getMonth();
-	int getDay();
+		int getYear();
+		int getMonth();
+		int getDay();
 
-	Date reDateculate ();
+		Date reDateculate ();
 
-	Date operator+ (int offset);
-	Date operator- (int offset);
+		Date operator+ (int offset);
+		Date operator- (int offset);
 
-	/*Date operator+= (int offset);
+		Date operator+= (int offset);
 
-	//Compare
-	bool operator== (const Date &Date2);
-	bool operator!= (const Date &Date2);
+		bool operator== (const Date &Date2);
+		bool operator!= (const Date &Date2);
 
-	int operator- (const Date &Date1, const Date &Date2);
-
-	//Show
-	//friend outstream& operator<<(ostream &out, const Date &old_Date);
-	*/
-
-	void show ();
+		void show ();
 
 };
 
