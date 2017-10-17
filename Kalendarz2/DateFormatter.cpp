78307e8efcbdd7ece8e5b581ce4e8c2191d60151
MonikaKrol <krol.monika.k@gmail.com>
2017-10-17 17:00:29 +0200

    Update DateFormatter.cpp

--- Kalendarz2/DateFormatter.cpp
+++ Kalendarz2/DateFormatter.cpp
@@ -1,33 +1,29 @@
-//
-// Created by Damian on 15.10.2017.
-//
-
+//potrzebne?
 #include "DateFormatter.h"
 
+string German::format(Date date) {
+	int y = date.getYear();
+	int m = date.getMonth();
+	int d = date.getDay();
 
-string German::format(Cal date) {
-    int y = date.getYear();
-    int m = date.getMonth();
-    int d = date.getDay();
+	char s[11];
+	sprintf(s, "%02d.%02d.%04d\n", d, m, y);
 
-    char s[11];
-    sprintf(s, "%02d.%02d.%04d\n", d, m, y);
+	string to_print(s);
 
-    string to_print(s);
-
-    return to_print;
+	return to_print;
 }
 
 
-string Computer::format(Cal date) {
-    int y = date.getYear();
-    int m = date.getMonth();
-    int d = date.getDay();
+string Computer::format(Date date) {
+	int y = date.getYear();
+	int m = date.getMonth();
+	int d = date.getDay();
 
-    char s[11];
-    sprintf(s, "%04d-%02d-%02d\n", y, m, d);
+	char s[11];
+	sprintf(s, "%04d-%02d-%02d\n", y, m, d);
 
-    string to_print(s);
+	string to_print(s);
 
-    return to_print;
+	return to_print;
 }
